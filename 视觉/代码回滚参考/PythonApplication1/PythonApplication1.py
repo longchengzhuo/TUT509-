ddba3e9566cb71032dbfae14d2b3ef708f8a84c2
longchengzhuo <89187533+longchengzhuo@users.noreply.github.com>
2022-09-14 21:23:01 +0800

    Add files via upload

---
+++ 视觉/代码回滚参考/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,150 @@
+import cv2
+import time
+import math
+
+cam = cv2.VideoCapture(0)
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))  # 定义结构元素
+
+sightX = 330
+sightY = 245
+
+while(True):
+
+    timeStamp = time.time()
+
+    print("")
+    print("--New F:")
+    ret, img = cam.read()  # 获取一帧图像
+    #img = cv2.imread("testImg.png")
+
+    blueImg, greenImg, redImg = cv2.split(img)  # 分离图像的RGB通道
+    img2 = cv2.subtract(blueImg, redImg)  # B通道-R通道
+    img2 = cv2.subtract(img2, greenImg)  # (B通道-R通道)-G通道
+
+    ret, img2 = cv2.threshold(img2, 50, 255, cv2.THRESH_BINARY)  # 图像二值化处理
+    img2 = cv2.morphologyEx(img2, cv2.MORPH_OPEN, kernel)  # 开运算
+    # img2 = cv2.dilate(img2, kernel）        #膨胀处理
+    contours, hierarchy = cv2.findContours(
+        img2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # 获取轮廓
+    cv2.drawContours(img, contours, -1, (0, 0, 255), 2)  # 在原始图像上绘制轮廓以进行显示
+
+    n = 0
+    x = []
+    y = []
+    longSide = []
+    shortSide = []
+    angle = []  # 初始化变量
+    for contour in contours:
+        rect = cv2.minAreaRect(contours[n])  # 获取最小包围矩形
+        xCache, yCache = rect[0]  # 获取矩形的中心坐标
+        widthC, heigthC = rect[1]  # 获取矩形的长宽
+        angleCache = rect[2]  # 获取矩形的角度 (-90, 0]
+
+        if heigthC > widthC:
+            if heigthC / widthC >= 2.1:  # 长宽比满足条件
+                # if widthC < heigthC and heigthC / widthC >= 2.1:        #灯条是竖直放置，长宽比满足条件
+                x.append(int(xCache))
+                y.append(int(yCache))
+                longSide.append(int(heigthC))
+                # shortSide.append(widthC)
+                angle.append(angleCache)
+                print("·X:", xCache)
+                print(" Y:", yCache)
+                print(" Long:", heigthC)
+                print(" Short:", widthC)
+                n = n + 1  # 有效矩形计数
+        else:
+            if widthC / heigthC >= 2.1:  # 长宽比满足条件
+                x.append(int(xCache))
+                y.append(int(yCache))
+                longSide.append(int(widthC))
+                # shortSide.append(heigthC)
+                angle.append(angleCache + 90)
+                print("·X:", xCache)
+                print(" Y:", yCache)
+                print(" Long:", widthC)
+                print(" Short:", heigthC)
+                n = n + 1  # 有效矩形计数
+
+    target = []  # 存储配对的两个灯条的编号 (L1, L2)
+    locX = []
+    locY = []  # 存储计算得到的中心点坐标
+    dis = []  # 存储中心点与准星的距离
+    pairNum = 0  # 初始化计数变量
+    if n >= 2:  # 图像中找到两个以上的灯条
+        for count in range(0, n):
+            findCache = count + 1  # 初始化计数变量
+            while findCache < n:  # 未超界，进行匹配运算
+                calcCache = math.sqrt(
+                    (x[findCache] - x[count]) ** 2 + (y[findCache] - y[count]) ** 2)  # 求中心点连线长
+                calcCache = calcCache / \
+                    (longSide[count] + longSide[findCache])  # 求快捷计算单位
+                print("·Scale:", calcCache)
+                if abs(angle[count] - angle[findCache]) < 10 and (1.0 < calcCache < 1.4):  # 满足匹配条件
+                    # if abs(angle[count] - angle[findCache]) < 10 and (0.8 < calcCache < 1.2 or 1.8 < calcCache < 5.2):  #满足匹配条件
+                    target.append((count, findCache))
+                    #loc.append((int((x[count] + x[findCache]) / 2), int((y[count] + y[findCache]) / 2)))
+                    locX.append(int((x[count] + x[findCache]) / 2))
+                    locY.append(int((y[count] + y[findCache]) / 2))
+                    dis.append(
+                        math.sqrt((locX[pairNum] - sightX) ** 2 + (locY[pairNum] - sightY) ** 2))
+                    cv2.circle(
+                        img, (locX[pairNum], locY[pairNum]), 3, (0, 0, 255), -1)
+                    # 画两个圆来显示中心点的位置
+                    cv2.circle(
+                        img, (locX[pairNum], locY[pairNum]), 8, (0, 0, 255), 2)
+                    print("··Group", pairNum, ":")
+                    print("·L1:", target[pairNum][0])
+                    print("·L2:", target[pairNum][1])
+                    print("")
+
+                    pairNum = pairNum + 1  # 计数变量自增
+                findCache = findCache + 1
+
+        # ···在此处添加数字验证真假装甲板代码
+
+        if pairNum != 0:
+            # 寻找离准星最近的装甲板
+            disCalcCache = dis[0]
+            targetNum = 0  # 存储距离准星做进的装甲板编号
+            for count in range(0, pairNum):
+                if dis[count] < disCalcCache:
+                    targetNum = count
+                    disCalcCache = dis[count]
+
+            print("···FIND TARGET !!!")
+            print("·Target:", targetNum)
+            print("X:", locX[targetNum])
+            print("Y:", locY[targetNum])
+            cv2.line(img, (locX[targetNum], 0),
+                     (locX[targetNum], 480), (0, 255, 0), 1)  # 画竖线
+            cv2.line(img, (0, locY[targetNum]),
+                     (720, locY[targetNum]), (0, 255, 0), 1)  # 画横线
+            cv2.line(img, (x[target[targetNum][0]], y[target[targetNum][0]]), (
+                x[target[targetNum][1]], y[target[targetNum][1]]), (0, 255, 255), 2)  # 画连接线
+            cv2.line(img, (locX[targetNum], locY[targetNum]),
+                     (sightX, sightY), (255, 0, 255), 2)  # 画指向线
+
+    # 画准星
+    cv2.line(img, (sightX, sightY - 4),
+             (sightX, sightY - 9), (50, 250, 100), 1)  # 上
+    cv2.line(img, (sightX, sightY + 4),
+             (sightX, sightY + 9), (50, 250, 100), 1)  # 下
+    cv2.line(img, (sightX - 4, sightY),
+             (sightX - 9, sightY), (50, 250, 100), 1)  # 左
+    cv2.line(img, (sightX + 4, sightY),
+             (sightX + 9, sightY), (50, 250, 100), 1)  # 右
+
+    timeStamp = time.time() - timeStamp + 0.0001
+    fpsCalc = int(100 / timeStamp) / 100.0
+    cv2.putText(img2, str(timeStamp), (0, 30),
+                cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 255, 255), 1, 4)
+    cv2.putText(img2, str(fpsCalc), (0, 50),
+                cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 255, 255), 1, 4)
+    cv2.putText(img2, 'FPS MAX', (50, 50),
+                cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255, 255, 255), 1, 4)
+    cv2.imshow('raw', img)
+    cv2.imshow('ter', img2)  # 进行显示
+
+    if cv2.waitKey(1) == ord('1'):
+        break
---
+++ 视觉/代码回滚参考/PythonApplication1/config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+# 内参数矩阵
+Camera_intrinsic = {
+
+    "mtx": np.array([[1.35359275e+03, 0., 9.83995246e+02], [0., 1.38391379e+03, 5.96111551e+02], [0., 0., 1.]],
+                    dtype=np.double),
+    "dist": np.array([[-7.82876538e-03, -1.62051637e-01, -2.26163020e-02, 2.48180843e-04, 8.56086868e-01]], dtype=np.double),
+
+}
